085dfa1a5da08cc1918010d4197dfcf4f7d08484
HanielUlises <hanielulises2003@gmail.com>
2026-03-18 23:34:13 -0600

    Removing race condition

--- Concurrency & Parallelism/Threads/OMP/reduction.c
+++ Concurrency & Parallelism/Threads/OMP/reduction.c
@@ -1,14 +1,41 @@
 #include <omp.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#define N 1000
 
 int main() {
-    int sum = 0;
+    double **matrix;
+    double total_sum = 0.0;
 
-    #pragma omp parallel for reduction(+:sum)
-    for (int i = 0; i < 1000000; i++) {
-        sum += i;  // Safe
+    // Allocate matrix
+    matrix = (double **)malloc(N * sizeof(double *));
+    for (int i = 0; i < N; i++) {
+        matrix[i] = (double *)malloc(N * sizeof(double));
     }
 
-    printf("Sum = %d\n", sum);
+    // Initialize matrix
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            matrix[i][j] = (double)(i + j);
+        }
+    }
+
+    // Parallel sum with reduction
+    #pragma omp parallel for collapse(2) reduction(+:total_sum)
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            total_sum += matrix[i][j];
+        }
+    }
+
+    printf("Total sum = %.2f\n", total_sum);
+
+    // Free memory
+    for (int i = 0; i < N; i++) {
+        free(matrix[i]);
+    }
+    free(matrix);
+
     return 0;
 }
