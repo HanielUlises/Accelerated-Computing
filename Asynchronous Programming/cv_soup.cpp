09e89631851543b261b068b2c3524e0f14e19e34
HanielUlises <hanielulises2003@gmail.com>
2026-03-30 13:11:14 -0600

    Condition vars

---
+++ Asynchronous Programming/cv_soup.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <condition_variable>
+#include <thread>
+#include <mutex>
+
+int soup_servings = 10;
+std::mutex slow_cooker_lid;
+std::condition_variable soup_taken;
+
+void hungry_person(int id) {
+    int put_lid_back = 0;
+    while (soup_servings > 0) {
+        std::unique_lock<std::mutex> lid_lock(slow_cooker_lid);
+        while((id != soup_servings % 2) && (soup_servings > 0)) {
+            put_lid_back++;
+            soup_taken.wait(lid_lock);
+        }        
+
+        if(soup_servings > 0) {
+            soup_servings--;
+            lid_lock.unlock();
+            soup_taken.notify_one();
+        }
+    }
+    std::cout << "Person " << id << " put the lid back " << put_lid_back << " times" << '\n';
+}
+
