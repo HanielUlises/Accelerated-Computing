71643c96321811a1fa5a1f3a322017b765bc35b4
HanielUlises <hanielulises2003@gmail.com>
2026-03-31 22:39:19 -0600

    Barriers on race conditions

--- Concurrency & Parallelism/Mutual Exclusion/read_write.cpp
+++ Concurrency & Parallelism/Mutual Exclusion/read_write.cpp
@@ -1,40 +1,40 @@
-#include <thread>
 #include <chrono>
 #include <iostream>
 #include <mutex>
 #include <shared_mutex>
+#include <thread>
 #include <vector>
 
 std::shared_mutex shrd_mut;
 
 unsigned int x = 0;
 
-void write(){
-    std::lock_guard<std::shared_mutex> lck_guard(shrd_mut);
-    x++;
+void write() {
+  std::lock_guard<std::shared_mutex> lck_guard(shrd_mut);
+  x++;
 }
 
 void read() {
-    std::shared_lock<std::shared_mutex> lck_guard(shrd_mut);
-    using namespace std::literals;
-    std::this_thread::sleep_for(100ms);
+  std::shared_lock<std::shared_mutex> lck_guard(shrd_mut);
+  using namespace std::literals;
+  std::this_thread::sleep_for(100ms);
 }
 
 int main() {
-    std::vector<std::thread> threads;
+  std::vector<std::thread> threads;
 
-    for(int i = 0; i < 20; i++){
-        threads.emplace_back([](){read();});
-    }
+  for (int i = 0; i < 20; i++) {
+    threads.emplace_back([]() { read(); });
+  }
 
-    threads.emplace_back([](){write();});
-    threads.emplace_back([](){write();});
+  threads.emplace_back([]() { write(); });
+  threads.emplace_back([]() { write(); });
 
-    for(int i = 0; i < 20; i++){
-        threads.emplace_back([](){read();});
-    }
+  for (int i = 0; i < 20; i++) {
+    threads.emplace_back([]() { read(); });
+  }
 
-    for(auto &thr: threads){
-        thr.join();
-    }
+  for (auto &thr : threads) {
+    thr.join();
+  }
 }
