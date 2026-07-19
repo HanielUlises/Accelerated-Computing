0d035dcabacbd9f984b732860aafcc83643b4eb6
HanielUlises <hanielulises2003@gmail.com>
2026-07-18 23:29:23 -0600

    C++20 Jthread

--- Concurrency & Parallelism/Threads/jthread_2.cpp
+++ Concurrency & Parallelism/Threads/jthread_2.cpp
@@ -1,7 +1,18 @@
 #include <thread>
 #include <iostream>
+#include <stop_token>
+#include <chrono>
 
-void assigned_job() {
+using namespace::std::literals;
+
+void assigned_job(std::stop_token token) {
+    int counter{0};
+    while(counter < 10) {
+        if(token.stop_requested()) return;
+        std::this_thread::sleep_for(0.2s);
+        std::cout << "This is an interruptible thread: " << counter << "\n";
+        ++counter;
+    }
     std::cout << "Task: [" << std::this_thread::get_id() << "]\n";
 }
 
